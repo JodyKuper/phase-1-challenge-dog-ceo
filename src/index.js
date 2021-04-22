1fe54c6a9c6f8d4e2dedbf556b5d4fc29e358f1f
Joel Cooper <jodycooper16@gmail.com>
2021-04-22 11:26:44 -0700

    commit tree ea4878a7c53367e258f001e683e2da16bac43317

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4" 
+
+const dogImage = document.getElementById('dog-image-container')
+
+const fetchImage = () =>{
+ return (fetch(imgUrl)
+.then(response => response.json())
+.then(dog => console.log))
+
+
+// .then(dog => fetchedImage(json)) 
+// .then(dog => { console.log(json)
+// })
+// .then(dog => console.log(json))
+}
+// debugger
+
+const fetchedImage =(img) => {
+    const dogA = document.createElement('img')
+    
+    dogImage.appendchild(dogA)
+
+}
